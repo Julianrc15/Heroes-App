1c9253ae3aee18deb78d3893fd33837819f1784e
Julianrc15 <jarc2802@gmail.com>
2022-05-05 12:05:27 -0500

    se crea estructura proyecto HeroesApp

---
+++ src/HeroesApp.js
@@ -0,0 +1,10 @@
+
+
+export const HeroesApp = () => {
+  return (
+    <div>
+        <h1>Heroes App</h1>
+        <hr/>
+    </div>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM, { createRoot } from 'react-dom/client';
+import { HeroesApp } from './HeroesApp';
+
+const root = createRoot(document.getElementById("root"));
+root.render(<HeroesApp />);
+
+
