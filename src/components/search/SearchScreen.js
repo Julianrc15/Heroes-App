5e195793d51b8a3187460943ef3de305b4441633
Julian <jarc2802@gmail.com>
2022-05-13 12:17:07 -0500

    Se crea componente de busqueda y querys para filtrar por nombre

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,16 +1,31 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import queryString from 'query-string'
+
 import { useForm } from "../../hooks/useForm"
+import { getHeroesByName } from "../selectors/getHeroesByName";
+import {HeroCard} from '../hero/HeroCard'
+
+
 
 export const SearchScreen = () => {
 
+  const navigate= useNavigate()
+  const location = useLocation() 
+
+  const {q=''} =queryString.parse(location.search);
+
+  
+
   const [formValues,handleInputChange]=useForm({
-    searchText: '',
+    searchText: q,
   })
 
   const {searchText} = formValues;
-
+  const heoresFiltered = getHeroesByName(q) 
   const handleSearch=(e)=>{
     e.preventDefault();
     console.log(searchText);
+    navigate(`?q=${searchText}`)
  
   }
   
@@ -40,6 +55,28 @@ export const SearchScreen = () => {
                  </button>
               </form>
             </div>
+          
+          <div className="col-7">
+            <h3>Results</h3>
+            <hr/>
+            {
+              heoresFiltered.map(hero=>(
+                <HeroCard
+                key={hero.id}
+                {...hero}
+
+              />
+                            
+              )
+              ) 
+              
+            
+              
+            }
+
+
+          </div>
+
           </div>
       </>
     )
